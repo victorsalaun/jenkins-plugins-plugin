46aa81467be613ce1e9bfff78f0cdf8907e12122
victorsalaun <victor.salaun@gmail.com>
2018-05-30 23:45:19 +0200

    display as table and sort

--- src/main/java/com/victorsalaun/pluginsplugin/PluginsPlugin.java
+++ src/main/java/com/victorsalaun/pluginsplugin/PluginsPlugin.java
@@ -7,7 +7,9 @@ import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.workflow.libs.GlobalLibraries;
 import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Extension
 public class PluginsPlugin implements UnprotectedRootAction {
@@ -30,11 +32,11 @@ public class PluginsPlugin implements UnprotectedRootAction {
     }
 
     public List<PluginWrapper> getPluginsList() {
-        return Jenkins.getActiveInstance().getPluginManager().getPlugins();
+        return Jenkins.getActiveInstance().getPluginManager().getPlugins().stream().sorted(Comparator.comparing(PluginWrapper::getDisplayName, String.CASE_INSENSITIVE_ORDER)).collect(Collectors.toList());
     }
 
     public List<LibraryConfiguration> getSharedLibrariesList() {
-        return GlobalLibraries.get().getLibraries();
+        return GlobalLibraries.get().getLibraries().stream().sorted(Comparator.comparing(LibraryConfiguration::getName, String.CASE_INSENSITIVE_ORDER)).collect(Collectors.toList());
     }
 
 }
